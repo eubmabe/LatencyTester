37baa831973649952de54cf85fe7755e618bc5bd
Mats Bergman <mats.bergman@teliasonera.com>
2016-10-16 21:14:01 +0200

    Test files added

---
+++ test/soundtest.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 16 21:05:28 2016
+
+@author: Mats
+"""
+
+
+import math
+import pyaudio
+
+#sudo apt-get install python-pyaudio
+PyAudio = pyaudio.PyAudio
+
+#See http://en.wikipedia.org/wiki/Bit_rate#Audio
+BITRATE = 16000 #number of frames per second/frameset.      
+
+#See http://www.phy.mtu.edu/~suits/notefreqs.html
+FREQUENCY = 261.63 #Hz, waves per second, 261.63=C4-note.
+LENGTH = 1.2232 #seconds to play sound
+
+NUMBEROFFRAMES = int(BITRATE * LENGTH)
+RESTFRAMES = NUMBEROFFRAMES % BITRATE
+WAVEDATA = ''    
+
+for x in xrange(NUMBEROFFRAMES):
+ WAVEDATA = WAVEDATA+chr(int(math.sin(x/((BITRATE/FREQUENCY)/math.pi))*127+128))    
+
+#fill remainder of frameset with silence
+for x in xrange(RESTFRAMES): 
+ WAVEDATA = WAVEDATA+chr(128)
+
+p = PyAudio()
+stream = p.open(format = p.get_format_from_width(1), 
+                channels = 1, 
+                rate = BITRATE, 
+                output = True)
+stream.write(WAVEDATA)
+stream.stop_stream()
+stream.close()
+p.terminate()
+
