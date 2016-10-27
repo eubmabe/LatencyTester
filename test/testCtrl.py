805015ca1b3bd52245fc11bcb2fe554bbad3f2e4
Mats Bergman <mats.bergman@teliasonera.com>
2016-10-27 02:33:15 +0200

    New testprogram with test control object

---
+++ test/testCtrl.py
@@ -0,0 +1,127 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 26 22:48:09 2016
+
+@author: Mats
+"""
+import pyaudio
+import wave
+import time
+import numpy as np
+
+class testCtrl:
+    
+    def __init__(self,defaultSoundFile='test.wav',NOISE_FACTOR=20,PULSE_DETECT_SAMP=1000):
+        self.recordedData = []
+        self.NOISE_FACTOR=NOISE_FACTOR
+        self.PULSE_DETECT_SAMP=PULSE_DETECT_SAMP
+        self.callBackCompleted = False
+        self.noiseLevel = (0.0,0.0)
+        self.p = pyaudio.PyAudio()
+        self.defaultSoundFile = defaultSoundFile
+        self.callBackFunctionPtr = None
+        self.wf = wave.open(defaultSoundFile, 'rb') # wf part of class to simplify clean up
+        self.bytesPerSample = self.wf.getsampwidth()*self.wf.getnchannels()
+        
+        self.stream = self.p.open(format=self.p.get_format_from_width(self.wf.getsampwidth()),
+                    channels=self.wf.getnchannels(),
+                    rate=self.wf.getframerate(),
+                    output=True,
+                    input = True,
+                    stream_callback=self.callBackFunction)
+        
+        self.stream.start_stream()
+        self.wf.close()
+        self.wf = None # wf used to get correct audio file data
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.stream.stop_stream()
+        self.stream.close()
+        if self.wf is not None:
+            self.wf.close()
+        self.p.terminate()
+        print 'Test ctrl destructed!!!'
+        
+    def waitForCallbackCompleted (self, timeout,callBack=None): # Timeout in number of loops
+        loop_cnt =0
+        while self.stream.is_active() and loop_cnt < timeout and not self.callBackCompleted:
+            time.sleep(1)
+            if callBack is not None:
+                retVal = callBack (loop_cnt,timeout)
+            else:
+                retVal = None
+            if retVal is None:
+                loop_cnt += 1
+            else:
+                loop_cnt = retVal
+
+
+    def setCallBackFunction (self,callBackPtr):
+        self.callBackFunctionPtr = callBackPtr
+        
+    def callBackFunction(self,in_data, frame_count, time_info, status):
+        data=''
+        returnCode = pyaudio.paContinue
+        if self.callBackFunctionPtr is not None:
+            (data,returnCode,self.callBackCompleted) = self.callBackFunctionPtr (in_data, self.recordedData, frame_count, time_info, status)
+        
+        data += '\x00'*(frame_count*self.bytesPerSample-len (data))
+        return (data, returnCode)
+ 
+    def playSoundCallBack (self, in_data, recordedData, frame_count, time_info, status):
+        data = self.wf.readframes(frame_count)
+        if data == '':
+            return (data, pyaudio.paContinue,True)
+        else:          
+            return (data, pyaudio.paContinue,False)
+        
+    def playSound (self,maxTime,waveSrc):
+        self.wf = wave.open(waveSrc, 'rb')
+        self.setCallBackFunction (self.playSoundCallBack)
+        self.waitForCallbackCompleted(maxTime,self.detectPulses)
+        self.setCallBackFunction(None)
+        
+        
+    def measureNoiseCallBack (in_data, recordedData, frame_count, time_info, status):
+        dataChunk = np.abs(np.fromstring(in_data, dtype='int{0}'.format(16)))
+        dataChunk.shape = [frame_count,2]
+        recordedData.append (dataChunk)
+        return ('', pyaudio.paContinue,False)
+        
+    def measureNoiseLevel (self,testTime):
+        self.setCallBackFunction (self.measureNoiseCallBack)
+        time.sleep(testTime)
+        self.setCallBackFunction(None)
+        recordedDataVec = np.reshape(self.recordedData,newshape = [-1,2])
+        self.noiseLevel = recordedDataVec.mean(axis=0)
+        print "Noise level = " + str(self.noiseLevel)
+        
+    def measureCallDelayCallBack (self, in_data, recordedData, frame_count, time_info, status):
+        dataChunk = np.abs(np.fromstring(in_data, dtype='int{0}'.format(16)))
+        dataChunk.shape = [frame_count,2]
+        recordedData.append (dataChunk)
+
+        data = self.wf.readframes(frame_count)
+        
+        return (data, pyaudio.paContinue,False)
+        
+    def measureCallDelay (self,testTime,waveSrc):
+        self.wf = wave.open(waveSrc, 'rb')
+        self.setCallBackFunction (self.measureCallDelayCallBack)
+        self.waitForCallbackCompleted(testTime,self.detectPulses)
+        time.sleep(0.5)
+        self.setCallBackFunction(None)
+        recordedDataVec = np.reshape(self.recordedData,newshape = [-1,2])
+        np.save ('outNP.npy',recordedDataVec)
+        print "Delay measurement done!!"
+        
+    def detectPulses (self,currTime,endTime):
+        recordedDataVec = np.reshape(self.recordedData,newshape = [-1,2])
+        detectVec = (recordedDataVec>self.noiseLevel*self.NOISE_FACTOR).sum(axis=0)
+        if (detectVec>self.PULSE_DETECT_SAMP).sum() == 2:
+            print "Pulse on both channels detected!!!"
+            return endTime
+        return None
---
+++ test/testsequence2.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 22 03:44:25 2016
+
+@author: Mats
+"""
+
+import RPi.GPIO as GPIO
+import testCtrl as TC
+import time
+import sys
+
+
+    
+
+click_delay = 0.5
+if len(sys.argv) >=2:
+    click_delay = float(sys.argv[1])
+print("Click delay = " + str (click_delay))
+
+
+# Init test bed
+# instantiate testObject
+with TC.testCtrl(defaultSoundFile='test.wav',NOISE_FACTOR=20,PULSE_DETECT_SAMP=1000) as testObj:
+    #testObj = TC.testCtrl(defaultSoundFile='test.wav',NOISE_FACTOR=20,PULSE_DETECT_SAMP=1000)
+    
+    print 'Init GPIO'
+    channel = 12 #pin #12 used
+    GPIO.setmode(GPIO.BOARD)
+    # Mic button not pressed GPIO.HIGH activate button
+    GPIO.setup(channel, GPIO.OUT, initial=GPIO.LOW)
+    time.sleep(2)
+    
+    # Press button 2 sec (left channel)
+    print 'Press mic button'
+    GPIO.output(channel, GPIO.HIGH)
+    # Wait 2 seconds and release
+    time.sleep(2)
+    print 'Release mic button'
+    GPIO.setup(channel, GPIO.OUT, initial=GPIO.LOW)
+    
+    
+    
+    # Wait a couple of seconds and play "command" to set-up call via voice recognition
+    time.sleep(3)
+    print 'Play call test number on left channel'
+    
+    testObj.playSound (10,'CallB_Left.wav')
+    
+    print 'Command played'
+    #Wait for call set-up to complete
+    # Can be improved by sending sound from phone B
+    # But let's keep it simple to start with
+    time.sleep(15)
+    
+    print 'Play test sound'
+    testObj.playSound (10,'test.wav')
+    time.sleep(5)
+    print 'Start test'
+    testObj.measureNoiseLevel(5)
+    testObj.measureCallDelay(10,'testPulseLeft.wav')
+    
+    print 'COMPLETED!!!!'
+    
+    
+    print 'Press mic button'
+    GPIO.output(channel, GPIO.HIGH)
+    # Wait 2 seconds and release
+    time.sleep(click_delay)
+    print 'Release mic button'
+    GPIO.setup(channel, GPIO.OUT, initial=GPIO.LOW)
+    
+    time.sleep(1) # Give time to sence LOW
+
+
+GPIO.cleanup()
